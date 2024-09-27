3ab7faa60cda46674e803c0882f9db41d9d5a0cc
msalmankhokhar <kjokhars@gmail.com>
2024-09-27 21:30:32 +0500

    used driver.execute_script() instead of element.sendkeys()

--- driver.py
+++ driver.py
@@ -4,4 +4,11 @@ from vars import login_URL
 
 chrome_driver_path = 'webdrivers/chromedriver.exe'
 
-driver = webdriver.Chrome(service=ChromeService(executable_path=chrome_driver_path))
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('useAutomationExtension', False)
+chrome_options.add_argument("window-size=1200x600")
+
+driver = webdriver.Chrome(
+    service=ChromeService(executable_path=chrome_driver_path),
+    options=chrome_options
+    )
--- login.py
+++ login.py
@@ -7,22 +7,31 @@ from vars import email, password
 from utility import notify
 from time import sleep
 
-email_input_selector = '#UserId6'
-password_input_selector = '#Password1'
+email_input_selector = 'input[type="text"]'
+password_input_selector = 'input[type="password"]'
 login_btn_selector = '#btnSubmit'
 verify_btn_selector = '#btnVerify'
 
 if __name__ == "__main__":
     driver.get(login_URL)
-    driver.find_element(By.CSS_SELECTOR, email_input_selector).send_keys(email)
-    driver.find_element(By.CSS_SELECTOR, password_input_selector).send_keys(password)
-    driver.find_element(By.CSS_SELECTOR, verify_btn_selector).click()
-    notify('Please solve captcha in 20 seconds')
-    sleep(20)
-    driver.find_element(By.CSS_SELECTOR, login_btn_selector).click()
-    notify('Logged in successfully')
-    sleep(20)
-
-
-    
-
+    sleep(5)
+    emailInput = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, email_input_selector)),
+        message="Email input not found"
+    )
+    passwordInput = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, password_input_selector)),
+        message="Password input not found"
+    )
+    verifyBtn = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, verify_btn_selector)),
+        message="Verify Button not found"
+    )
+    loginBtn = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, login_btn_selector)),
+        message="Login Btn not found"
+    )
+    driver.execute_script('arguments[0].setAttribute("value", arguments[1]);', emailInput, email)
+    driver.execute_script('arguments[0].setAttribute("value", arguments[1]);', emailInput, password)
+    verifyBtn.click()
+    notify("Please solve captch in 10 seconds")
